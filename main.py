2488819eb06da9fcfd621674714a528f16fc4e43
Victor Vidigal Ribeiro <victorvidigal@gmail.com>
2021-11-01 09:55:59 -0300

    Criação do projeto com flask

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+api = Flask(__name__)
+
+if __name__ == "__main__":
+    api.run(debug=True)
+
+@api.route('/api')
+def ola_mundo():
+    return jsonify({'text': 'Olá Mundo'})
+
+@api.route('/api/hello_world')
+def hello_world():
+    return jsonify({'text': 'Hello World'})
